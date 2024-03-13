354297beb230c5ad58de740dc2e5ff51f861561e
Nathan <nathandenholm0@gmail.com>
2024-03-13 05:05:42 +0000

    adds pet sitting page

--- scripts/embeddedSigningForm.js
+++ scripts/embeddedSigningForm.js
@@ -3,7 +3,6 @@ const templateSlug = urlParams.get("template");
 const embeddedForm = document.getElementById("embedded-pdf-form");
 embeddedForm.setAttribute("data-src", `https://docuseal.co/s/${templateSlug}`);
 embeddedForm.addEventListener("completed", onEmbeddedFormSubmit);
-
 function onEmbeddedFormSubmit(event) {
   console.log("event", event);
 }
