afc09dd74b878af54de4eca631fc8f5c3c71fe68
Nathan <nathandenholm0@gmail.com>
2024-03-18 08:30:37 +0000

    done

--- scripts/captureUser.js
+++ scripts/captureUser.js
@@ -7,6 +7,7 @@ async function handleUserDetailsSubmit(event) {
   submitButtonElement.disabled = true;
   const formData = new FormData(event.target);
   const { email, formType } = Object.fromEntries(formData);
+
   try {
     const response = await fetch(
       "https://dajho7fii3vyp.cloudfront.net/createDocusealSubmission",
@@ -21,6 +22,7 @@ async function handleUserDetailsSubmit(event) {
     const { slug } = await response.json();
     window.location.href = `../pages/embedded-signing-form.html?template=${slug}`;
   } catch (error) {
+    console.log("errr", error);
     window.location.href = "../pages/error.html";
   }
 }
