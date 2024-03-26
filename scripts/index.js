e8356584dd88b1d1f36927c858277f40aeb34273
Nathan <nathandenholm0@gmail.com>
2024-03-26 12:11:15 +0000

    fix reviws desktop

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,23 @@ new Swiper(".swiper", {
   pagination: {
     el: ".swiper-pagination",
   },
+  breakpoints: {
+    // // when window width is >= 320px
+    // 320: {
+    //   slidesPerView: 2,
+    //   spaceBetween: 20,
+    // },
+    // // when window width is >= 480px
+    // 480: {
+    //   slidesPerView: 3,
+    //   spaceBetween: 30,
+    // },
+    // // when window width is >= 640px
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+  },
   spaceBetween: 24,
 });
 function initMap() {
